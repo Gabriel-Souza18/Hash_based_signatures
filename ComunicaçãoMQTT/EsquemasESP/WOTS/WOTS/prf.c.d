e56941a3ee81e7853f2387b4f47d3114c291157c
Gabriel-Souza18 <gabrielsisou@gmail.com>
2026-09-17 14:57:54 -0300

    Organizando pastas ESP

---
+++ ComunicaçãoMQTT/EsquemasESP/WOTS/WOTS/prf.c.d
@@ -0,0 +1,3 @@
+WOTS/prf.c.o: /home/gabriel/Documentos/Hash_based_signatures/WOTS/prf.c \
+ /home/gabriel/Documentos/Hash_based_signatures/WOTS/prf.h \
+ /home/gabriel/Documentos/Hash_based_signatures/SHA256/sha256.h
